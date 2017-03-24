933699c9608a8d80c5d436387b3a7f3a547098e3
Andrea Braschi <a.braschi@reply.it>
2017-03-24 15:37:44 +0100

    first commit.

---
+++ install.sh
@@ -0,0 +1,5 @@
+apt-get install apache2
+apt-get install mysql-server
+sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql
+sudo apt-get install php-zipstream
+cd song-manager; php composer.phar install
